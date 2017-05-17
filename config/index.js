cdc781d254d3bf61ab12fe49d25b749269e1f865
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-05-17 10:02:34 +0200

    Updates Node. Updates dependencies. Updates tests

--- config/index.js
+++ config/index.js
@@ -3,5 +3,5 @@
 module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   ENCRYPTOR_SECRET: process.env.ENCRYPTOR_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
-  SESSION_STORAGE_URL: process.env.SESSION_STORAGE_URL || 'https://tmp.storage.micro.t-fk.no'
+  SESSION_STORAGE_URL: process.env.SESSION_STORAGE_URL || 'https://tmp.storage.tjeneste.win'
 }
